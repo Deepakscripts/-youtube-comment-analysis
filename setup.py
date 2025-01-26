c52d2244984c92b041c9a5db033268d9ef4fbc2e
Deepak agrawal <aadeepak05@gmail.com>
2025-01-26 17:39:30 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project leverages the Cookiecutter Data Science template to create a structured workflow for analyzing YouTube comments. The goal is to extract insights from user-generated comments by performing tasks such as data collection, cleaning, sentiment analysis, and visualization. The analysis can help identify trends, user sentiments, and popular topics discussed in YouTube videos, offering valuable insights for creators, marketers, or researchers.',
+    author='Deepak Agrawal',
+    license='',
+)
